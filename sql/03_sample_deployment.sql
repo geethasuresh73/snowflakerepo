d579efa45512e46dd48c2aeec6b63a0bfc060fbd
Geetha <geethaskbaaa7255@gmail.com>
2026-01-13 22:20:53 -0500

    test

--- sql/03_sample_deployment.sql
+++ sql/03_sample_deployment.sql
@@ -27,4 +27,3 @@ create or replaceiceberg table s3tables_grocery
 use database DEMO_DBgeetha1;
 use schema DEMO_DBgeetha1.PUBLIC;
 Select count(*) from s3tables_grocery;
-drop table s3tables_grocery;
--- sql/04_sample_deployment.sql
+++ sql/04_sample_deployment.sql
@@ -27,4 +27,3 @@ create or replace iceberg table s3tables_grocery
 use database DEMO_DBgeetha2;
 use schema DEMO_DBgeetha2.PUBLIC;
 Select count(*) from s3tables_grocery;
-Drop table s3tables_grocery;
